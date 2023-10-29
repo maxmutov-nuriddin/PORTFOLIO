d67079d90691921e3ea4be02a59fe12baba0069f
maxmutov-nuriddin <mahmudovnuriddin35@gmail.com>
2023-10-29 17:28:22 +0500

    update Portfolio.tsx

--- portfolios/src/pages/admin/Portfolio.tsx
+++ portfolios/src/pages/admin/Portfolio.tsx
@@ -1,194 +1,3 @@
-// import { useContext, useEffect } from "react";
-// import request from "../../server/index";
-
-// import { Form, Input, Modal } from 'antd';
-
-// import Portfolio from "../../types/portfolio";
-
-// import { SearchContexts } from "../../context/Search";
-// import usePortfolio from "../../store/portfolio";
-// import PortfolioCard from "../../components/portfolioCard/PortfolioCard";
-
-
-// const Portfolios = () => {
-//   const { searchContext } = useContext(SearchContexts)
-
-//   const pageTotal = 9
-
-//   const {
-//     user,
-//     portfolio,
-//     loading,
-//     total,
-//     page,
-//     isModalOpen,
-//     modalLoading,
-//     selected,
-//     getPortfolio,
-//     setPage,
-//     controlModal,
-//     showModal,
-//     setModalLoading,
-//     setSelected,
-//     setSearch,
-//   } = usePortfolio();
-
-//   useEffect(() => {
-//     getPortfolio();
-//   }, [getPortfolio, user]);
-
-//   useEffect(() => {
-//     setSearch(searchContext);
-//   }, [searchContext]);
-
-//   const [form] = Form.useForm();
-
-//   const handleOk = async () => {
-//     try {
-//       setModalLoading(true);
-//       const values = await form.validateFields();
-//       if (selected === null) {
-//         await request.post("portfolios", values);
-//       } else {
-//         await request.put(`portfolios/${selected}`, values);
-//       }
-//       getPortfolio();
-//       controlModal(false);
-//       form.resetFields();
-//     } finally {
-//       setModalLoading(false);
-//     }
-//   };
-
-//   const editBtn = async (id: string) => {
-//     const { data } = await request.get(`portfolios/${id}`);
-//     form.setFieldsValue(data);
-//     controlModal(true);
-//     setSelected(id);
-//   }
-
-//   const deleteBtn = async (id: string) => {
-//     await request.delete(`portfolios/${id}`);
-//     getPortfolio();
-//   }
-
-//   if (loading) {
-//     return <div className='loading'>
-//       <div className="loading-wave">
-//         <div className="loading-bar"></div>
-//         <div className="loading-bar"></div>
-//         <div className="loading-bar"></div>
-//         <div className="loading-bar"></div>
-//       </div>
-//     </div>
-//   }
-
-//   const totalPages = Math.ceil(total / pageTotal);
-
-//   return (
-//     <div>
-//       <div className="search">
-//         <div className="totals ">
-//           {total}
-//         </div>
-//         <button className="btn btn-info" onClick={() => showModal(form)}>
-//           Add
-//         </button>
-//       </div>
-
-//       <div className="user__inner">
-//         {
-//           portfolio.map((portfolio) => <PortfolioCard key={portfolio._id} editBtn={editBtn} deleteBtn={deleteBtn} portfolio={portfolio} />)
-//         }
-//       </div>
-//       {totalPages > 1 && (
-//         <div className="skills__pagination">
-//           <button
-//             className="skills__pagination-button"
-//             disabled={page === 1}
-//             onClick={() => setPage(page - 1)}
-//           >
-//             Previous
-//           </button>
-//           <span className="skills__pagination-current">{page}</span>
-//           <button
-//             className="skills__pagination-button"
-//             disabled={page === totalPages}
-//             onClick={() => setPage(page + 1)}
-//           >
-//             Next
-//           </button>
-//         </div>
-//       )}
-
-//       <Modal
-//         title="Category data"
-//         maskClosable={false}
-//         confirmLoading={modalLoading}
-//         okText={selected === null ? "Add skill" : "Save skill"}
-//         open={isModalOpen}
-//         onOk={handleOk}
-//         onCancel={() => controlModal(false)}
-//       >
-//         <Form
-//           name="category"
-//           autoComplete="off"
-//           labelCol={{
-//             span: 24,
-//           }}
-//           wrapperCol={{
-//             span: 24,
-//           }}
-//           form={form}
-//         >
-//           <Form.Item<Portfolio>
-//             label="Name"
-//             name="name"
-//             rules={[
-//               {
-//                 required: true,
-//                 message: "Please fill!",
-//               },
-//             ]}
-//           >
-//             <Input />
-//           </Form.Item>
-
-//           <Form.Item<Portfolio>
-//             label="Url"
-//             name="url"
-//             rules={[
-//               {
-//                 required: true,
-//                 message: "Please fill!",
-//               },
-//             ]}
-//           >
-//             <Input />
-//           </Form.Item>
-
-//           <Form.Item<Portfolio>
-//             label="Description"
-//             name="description"
-//             rules={[
-//               {
-//                 required: true,
-//                 message: "Please fill!",
-//               },
-//             ]}
-//           >
-//             <Input />
-//           </Form.Item>
-
-//             <input type="file" />
-//         </Form>
-//       </Modal>
-//     </div>
-//   )
-// }
-
-// export default Portfolios
-
 import React, { useContext, useEffect, useState } from "react";
 import request from "../../server/index";
 import { Form, Input, Modal } from 'antd';
@@ -244,11 +53,7 @@ const Portfolios = () => {
     try {
       setModalLoading(true);
       const values = await form.validateFields();
-      if (selected === null) {
-        await request.post("portfolios", values);
-      } else {
-        await request.put(`portfolios/${selected}`, values);
-      }
+      console.log(values);
 
       if (selectedFile) {
         const formData = new FormData();
@@ -257,6 +62,12 @@ const Portfolios = () => {
         const response = await request.post("/upload", formData);
         values.photo = response.data;
         console.log(response);
+
+        if (selected === null) {
+          await request.post("portfolios", values);
+        } else {
+          await request.put(`portfolios/${selected}`, values);
+        }
 
         if (selected) {
           await request.put(`portfolios/${selected}`, values);
@@ -280,8 +91,6 @@ const Portfolios = () => {
     controlModal(true);
     setSelected(id);
   };
-
-
 
   const deleteBtn = async (id: string) => {
     await request.delete(`portfolios/${id}`);
