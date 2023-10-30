e45accc80b025c73d9b423cf0e236e3963923ca4
maxmutov-nuriddin <mahmudovnuriddin35@gmail.com>
2023-10-30 12:35:50 +0500

    update NavBar.tsx and profile.scss

--- portfolios/src/components/portfolioPage/NavBar.tsx
+++ portfolios/src/components/portfolioPage/NavBar.tsx
@@ -1,6 +1,55 @@
+import { SetStateAction, useEffect, useState } from "react";
+import { Nav, Navbar } from "react-bootstrap"
+
 const NavBar = () => {
+  const [activeLink, setActiveLink] = useState('home');
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const onScroll = () => {
+      if (window.scrollY > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    }
+
+    window.addEventListener("scroll", onScroll);
+
+    return () => window.removeEventListener("scroll", onScroll);
+  }, [])
+
+  const onUpdateActiveLink = (value: SetStateAction<string>) => {
+    setActiveLink(value);
+  }
+
   return (
-    <div>NavBar</div>
+    <>
+      <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
+        <Navbar.Brand className=" fw-bolder fs-1 text-light" href="/">
+          PORTFOLIO
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav">
+          <span className="navbar-toggler-icon"></span>
+        </Navbar.Toggle>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto me-3">
+            <Nav.Link href="#home" className={activeLink === 'home' ? 'navbar-link text-info ' : 'navbar-link text-light '} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
+            <Nav.Link href="#skills" className={activeLink === 'skills' ? 'navbar-link text-info ' : 'navbar-link text-light '} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
+            <Nav.Link href="#projects" className={activeLink === 'projects' ? 'navbar-link text-info ' : 'navbar-link text-light '} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
+          </Nav>
+          <span className="navbar-text">
+            <div className="social-icon">
+              <a className="social-icon-link" href="#">
+                <i className="bi fs-3 m-0 text-warning bi-instagram"></i>
+              </a>
+              <a className="social-icon-link" href="#"><i className="bi fs-3 m-0 text-warning bi-telegram"></i></a>
+              <a className="social-icon-link" href="#"><i className="bi fs-3 m-0 text-warning bi-github"></i></a>
+            </div>
+          </span>
+        </Navbar.Collapse>
+      </Navbar>
+    </>
   )
 }
 
